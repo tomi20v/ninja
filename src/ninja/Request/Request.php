0d30440f4ba1842d608f314a479cfb3b58fcc952
t <tomi20v@gmail.com>
2014-11-28 03:08:20 +0100

    added array of recognized extensions (html, json, js, etc) fixed uri extension ripping, requested extension can be in the middle of the uri

--- src/ninja/Request/Request.php
+++ src/ninja/Request/Request.php
@@ -10,21 +10,42 @@ namespace ninja;
 class Request extends \Symfony\Component\HttpFoundation\Request {
 
 	/**
-	 * @var string[] request uri parts without query string in a nice array. Will be consumed by shiftUriParts()
+	 * @var string[] original uri parts
+	 */
+	private $_uriParts = null;
+
+	/**
+	 * @var string[] request uri parts without query string and extension. will be copied when cloning and consumed by shiftUriParts()
 	 */
 	private $_remainingUriParts = null;
+
+	/**
+	 * @var string[] these extensions will be recognized
+	 */
+	protected $_routedExtensions = [
+		'html',
+		'json',
+		'js',
+		'css',
+		'xml',
+	];
 
 	/**
 	 * I return all request uri parts without query string in a nice array
 	 * @return array|string
 	 */
 	public function getUriParts() {
-		$uriParts = $this->getRequestUri();
-		if ($pos = strpos($uriParts, '?')) {
-			$uriParts = substr($uriParts, 0, $pos);
-		};
-		$uriParts = explode('/', trim($uriParts, '/'));
-		return $uriParts;
+		if (is_null($this->_uriParts)) {
+			$uriParts = $this->getRequestUri();
+			if ($pos = strpos($uriParts, '?')) {
+				$uriParts = substr($uriParts, 0, $pos);
+			};
+			if ($extension = $this->getRequestedExtension()) {
+				$uriParts = str_replace('.'.$extension, '', $uriParts);
+			}
+			$this->_uriParts = explode('/', trim($uriParts, '/'));
+		}
+		return $this->_uriParts;
 	}
 
 	/**
@@ -39,11 +60,27 @@ class Request extends \Symfony\Component\HttpFoundation\Request {
 	}
 
 	/**
-	 * @param int $count
+	 * I remove some parts from $this->_remainingUriParts
+	 * @param int|string|array $countOrParts count to shift, or parts to shift in string (slug) or array of its parts
 	 * @return $this
 	 */
-	public function shiftUriParts($count) {
-		$this->_remainingUriParts = array_slice($this->_remainingUriParts, $count);
+	public function shiftUriParts(&$countOrParts) {
+		if (is_string($countOrParts)) {
+			$countOrParts = explode('/', $countOrParts);
+		}
+		if (is_array($countOrParts)) {
+			do {
+				if (reset($countOrParts) !== reset($this->_remainingUriParts)) {
+					break;
+				}
+				array_shift($countOrParts);
+				array_shift($this->_remainingUriParts);
+			} while (count($countOrParts) && count($this->_remainingUriParts));
+		}
+		else {
+			$this->_remainingUriParts = array_slice($this->_remainingUriParts, intval($countOrParts));
+		}
+
 		return $this;
 	}
 
@@ -55,6 +92,17 @@ class Request extends \Symfony\Component\HttpFoundation\Request {
 	public function getClone() {
 		$NewRequest = clone $this;
 		return $NewRequest;
+	}
+
+	/**
+	 * @return string|null I recognize basic extensions to be considered (html, json, js, css, xml)
+	 */
+	public function getRequestedExtension() {
+		$pattern = '/.(' . implode('|', $this->_routedExtensions) . ')(\/.*|)$/';
+		if (preg_match($pattern, $this->getRequestUri(), $matches)) {
+			return $matches[1];
+		}
+		return null;
 	}
 
 }

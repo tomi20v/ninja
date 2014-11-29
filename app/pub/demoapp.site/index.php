01b3f81e8829c01738fb6a2bc794a74b67de3d58
t <tomi20v@gmail.com>
2014-11-29 03:03:52 +0100

    added snapshot testing

--- app/pub/demoapp.site/index.php
+++ app/pub/demoapp.site/index.php
@@ -10,6 +10,12 @@ define('PUB_ROOT', dirname(__FILE__));
 $Autoloader = require(PUB_ROOT . '/../../../vendor/autoload.php');
 \Finder::setAutoLoader($Autoloader);
 
+\Tester::init(
+	[
+		'ModAbstractModule' => ['getHmvcUrlPath'],
+	]);
+
+
 $t1 = microtime(true);
 $Maui = \Maui::instance(\Maui::ENV_DEFAULT, 'ninja');
 $Ninja = new \Ninja($Maui);
@@ -17,4 +23,5 @@ $Ninja->run();
 $t2 = microtime(true);
 
 //echop(($t2-$t0) . ' sec');
-//echop(($t2-$t1) . ' sec');
+$k = intval(memory_get_peak_usage()/1024);
+//echop(substr(($t2-$t1), 0, 5) . ' sec, ' . substr($k, 0, -3) . ',' . substr($k, -3) . 'K');
